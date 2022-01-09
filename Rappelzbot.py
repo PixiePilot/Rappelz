da25d50e481e0b6eec4c50d529b401eb1d821764
PixiePilot <89778100+PixiePilot@users.noreply.github.com>
2022-01-09 23:24:32 +0100

    Small rewrite
    I rewrote it to use discord commands instead of simply listening to chat ( this allows me to take in parameters ) 
    since I have been struggling with this

--- Rappelzbot.py
+++ Rappelzbot.py
@@ -1,71 +1,52 @@
 from logging import currentframe
 import os
 import discord
+from discord.ext import commands
 import time
 from dotenv import load_dotenv
 import pyodbc
 
-# loading constants from environment variables
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-#Reads the file that's defined below here, If it's in the same folder only add the name + extension)
-with open("Token.txt") as r:
-    DISCORD_TOKEN = (r.readline())
+
+connah = None # will later represent the connection to the Auth DB
+conn = None # will later represent the connection to the Telecaster DB
+connectionstring = ('Driver={SQL Server};'
+                    'Server=localhost;'
+                    'DataBase=Telecaster;'
+                    'Trusted_Connection=yes;')
+connectionstringah = ('Driver={SQL Server};'
+                    'Server=localhost;'
+                    'DataBase=Auth;'
+                    'Trusted_Connection=yes;')
+#^ The connection strings ^
 
 
-lasttime = 0
-conn = None # will later represent the connection to the database
+#loads the discord token.
+with open("Token.txt") as read:
+    DISCORD_TOKEN = (read.readline())
 
+bot = commands.Bot(command_prefix="!")
 
-# define discord bot
-client = discord.Client()
-
-@client.event
+#On_ready is code that's executed whenever the bot starts, In this case it will attempt to connect to the sql server.
+print("Attempting to connect to the SQL server...")
+@bot.event
 async def on_ready():
-    '''
-    this method is called when the bot is ready to do bot stuff
-    sets up a connection to the player database and logs status to console
-    '''
-    print('Connecting to SQL server.')
+    global conn,connah,connectionstring
+
     try:
-        global conn
-        conn = pyodbc.connect('Driver={SQL Server};'
-                              'Server=localhost;'
-                              'Database=Telecaster;'
-                              'Trusted_Connection=yes;')
-    except:
-        print('Couldn\'t connect to the SQL server.')
+        conn = pyodbc.connect(connectionstring)
+        connah = pyodbc.connect(connectionstringah)
+    except ConnectionRefusedError:
+        print('Connection refused by SQL server\nThis bot only works with Myssql make sure you\'re not using Mysql')
         quit()
-    print(f'Loaded successfully as {client.user}')
+    except ConnectionError:
+        print('Unable to connect to SQL server\nThis could be caused by :\nIncorrect password or user or invalid permissions to see the error')
+        quit()
+
+    print(f'successfully connected to SQL server.\nLogged in as user {bot.user} on discord')
 
 
-
-@client.event
-async def on_message(msg):
-    '''
-    on message recieve, process message.
-    this is the bulk of the bot's functionality
-    '''
-# Help command -------------------------------------------------------------
-    if msg.content == '!help':
-        await msg.channel.send('The current commands are:\n!gold\n!lvl')
-        return
-# Help command -------------------------------------------------------------
-
-
-#Time block ( Anything past here has a time block of 10 seconds if it has time.time() on the lasttime 
-# defined )
-    curenttime = time.time()
-    global lasttime
-    
-    if (lasttime + 10) >= (curenttime):
-        return
-#Time block------------------------------------
-
-
-#------------------------------------------<lvl>----------------------------------
-    if msg.content == '!lvl':
+#list of functions
+def functionlvl(msg):
         cursor = conn.cursor()
         cursor.execute('SELECT TOP(3) name , lv from dbo.Character order by lv DESC')
         data    = cursor.fetchall()
@@ -78,38 +59,67 @@ async def on_message(msg):
             level.append(row.lv)
 
 
-        for rank, (x, y) in enumerate(zip(name,level)):
+        for rank, (namestring, levelstring) in enumerate(zip(name,level)):
 
-            message = (f'{message}\nRank {rank+1}\nname : {x:10} level: {y}')
+            message = (f'{message}\nRank {rank+1}\nname : {namestring:10} level: {levelstring}')
         message = (f'```{message}```')
-        await msg.channel.send(message)
-        lasttime = time.time()
-        return
-#------------------------------------------</lvl>---------------------------------
+        return message
+#Funciton level is simply the function that shows the top 3 highest level players in the game and returns the value
 
+def functiongold(msg):
+    cursor = conn.cursor()
+    cursor.execute('SELECT TOP(3) name , gold from dbo.Character order by gold DESC')
+    data = cursor.fetchall()
+    name = []
+    gold = []
+    message = str()
 
-#------------------------------------------<Gold>---------------------------------
-    if msg.content == '!gold':
-        cursor = conn.cursor()
-        cursor.execute('SELECT TOP(3) name , gold from dbo.Character order by gold DESC')
-        data = cursor.fetchall()
-        name = []
-        gold = []
-        message = str()
+    for row in data:
+        name.append(row.name)
+        gold.append(row.gold)
 
-        for row in data:
-            name.append(row.name)
-            gold.append(row.gold)
+    for rank, (namestring, goldstring) in enumerate(zip(name, gold)):
 
-        for rank, (x, y) in enumerate(zip(name, gold)):
+        message = (f'{message}\nRank{rank+1:3}\n name: {namestring:10} ruppee: {goldstring:3}')
 
-            message = (f'{message}\nRank{rank+1:3}\n name: {x:10} ruppee: {y:3}')
+    message = f'```{message}```'
+        
+    return message
+#Funciton gold is simply the function that shows the top 3 richest players in the game and returns the value
 
-        message = f'```{message}```'
-        await msg.channel.send(message)
-        lasttime = time.time()
-        return
-#------------------------------------------</Gold>---------------------------------
+def discordidtoname(msg):
+        cursor = connah.cursor()
+        user = msg.author.id
+        cursor.execute(f"SELECT account,DiscordID FROM [Account] WHERE DiscordID ={user}")
+        result = cursor.fetchall()
+        username = []
+        count = 0
+        for row in result:
+            username.append(row.account)
+            count += 1
+            if count == 2:
+                msg.channel.send("You can only have one discord ID per account.")
+                return False
+        username = username.pop()
+        return username
+#Function to resolves the users account based on their discord ID ( Needs the ID to already be added to the AUTH db table design in varchar ( it's used as an int ) )
 
-#run discord bot
-client.run(DISCORD_TOKEN)
+@bot.command()
+async def lvl(msg):
+    message = functionlvl(msg)
+    await msg.channel.send(message)
+#functionlvl
+
+@bot.command()
+async def gold(msg):
+    message = functiongold(msg)
+    await msg.channel.send(message)
+#functiongold
+
+@bot.command()
+async def test(msg):
+    resolved_name = discordidtoname(msg)
+    await msg.channel.send(resolved_name)
+#function discordidtoname
+
+bot.run(DISCORD_TOKEN)
