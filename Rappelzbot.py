1b46360440930100d53bba31b41cf4d0df39af94
PixiePilot <89778100+PixiePilot@users.noreply.github.com>
2022-01-10 19:24:23 +0100

    Working on a automated event
    Added a feature for giveaways, It's not completed yet but it's a honest day of work ;p

--- Rappelzbot.py
+++ Rappelzbot.py
@@ -1,11 +1,14 @@
 from logging import currentframe
+import threading
 import os
 import discord
+from discord.client import Client
 from discord.ext import commands
 import time
 from dotenv import load_dotenv
 import pyodbc
-
+import asyncio
+giveawaylist = []
 
 connah = None # will later represent the connection to the Auth DB
 conn = None # will later represent the connection to the Telecaster DB
@@ -97,12 +100,18 @@ def discordidtoname(msg):
         for row in result:
             username.append(row.account)
             count += 1
-            if count == 2:
+            if count >= 2:
                 msg.channel.send("You can only have one discord ID per account.")
                 return False
         username = username.pop()
         return username
 #Function to resolves the users account based on their discord ID ( Needs the ID to already be added to the AUTH db table design in varchar ( it's used as an int ) )
+
+def threadingwait(waittime,msg,giveawaylist):
+    time.sleep(waittime)
+    print(giveawaylist)
+    return
+#Timer for giveaway after deletion, Since it's a multithread the sleep will not stop the rest of the code
 
 @bot.command()
 async def lvl(msg):
@@ -122,4 +131,33 @@ async def test(msg):
     await msg.channel.send(resolved_name)
 #function discordidtoname
 
+@bot.command()
+async def giveaway(msg,itemname: str,timer: int,id: int):
+        global giveawaymessage,giveawaylist
+        giveawaymessage = await msg.channel.send((f'There\'s a give away where {itemname} will be given away in {timer} seconds'))
+        emoji = '\N{THUMBS UP SIGN}'
+        await giveawaymessage.add_reaction(emoji)
+        print(giveawaymessage)
+        
+        #the await is ignored for the first one, When it's ran again after the timer it will run the code below it aswell.
+        await giveawaymessage.delete(delay=timer)
+        thread = threading.Thread(target=threadingwait,args=[timer,msg,giveawaylist])
+        thread.start()
+        print('test')
+
+    
+
+
+
+@bot.event
+async def on_reaction_add(reaction,user):
+    global giveawaymessage,giveawaylist
+    if reaction.message == giveawaymessage:
+        for check in giveawaylist:
+            if check == user.id:
+                return
+        giveawaylist.append(user.id)
+        print(giveawaylist)
+    return
+#Checks the current give away message for newly added ( does not save if bot shuts down)
 bot.run(DISCORD_TOKEN)
